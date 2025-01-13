41f12fd18d28d150f2d91bf9662cc26798fc0656
dober <iamyusein@gmail.com>
2025-01-13 20:56:13 +0200

    so far so good, if any upades i will come back!

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Smooth scrolling for navigation links
+    document.querySelectorAll('nav a[href^="#"]').forEach((link) => {
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
+            const target = document.querySelector(link.getAttribute("href"));
+            target.scrollIntoView({ behavior: "smooth" });
+        });
+    });
+    
+}
+
+
+);
+
+// Submission Alert Message
+
+const form = document.getElementById("contact");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        alert("Thank you for contacting us!");
+        form.reset();
+    });
